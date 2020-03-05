8122d722d4465ba644cf36d4ef2b3a0a0d25d6a2
AhmedHritani <40913833+AhmedHritani@users.noreply.github.com>
2020-03-05 09:19:57 +0200

    static

--- SySmsClas/urls.py
+++ SySmsClas/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
